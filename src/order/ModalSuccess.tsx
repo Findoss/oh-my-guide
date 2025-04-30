ffad08ccf7039d5c4da96d5b995808264952014a
Nikita Stroganov <findoss1@gmail.com>
2025-05-01 00:23:58 +0300

    fix button

--- src/order/ModalSuccess.tsx
+++ src/order/ModalSuccess.tsx
@@ -41,6 +41,7 @@ export const ModalSuccess = ({
         <div className="p-4 md:p-10 text-center">
           <h3 className="mb-5 text-lg font-normal">{text}</h3>
           <button
+            onClick={onChange}
             data-modal-hide="popup-modal"
             type="button"
             className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-green-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
