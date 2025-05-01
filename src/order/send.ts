81aa79f02e7b0cc36e04c951f4d53c31624b7ec0
Nikita Stroganov <findoss1@gmail.com>
2025-05-01 11:51:56 +0300

    update email style

--- src/order/send.ts
+++ src/order/send.ts
@@ -15,33 +15,27 @@ const formatEmail = (
 ) => {
   const textGridMouth = (gridMouth: GridMouth, stateMouth: StateMouth) => {
     const text = [];
-    text.push(`<table>`);
+    text.push(`<table border="1" cellspacing="0" cellpadding="0">`);
 
     gridMouth.forEach((row, rowIndex) => {
-      text.push('<tr style="border: 1px solid black">');
+      text.push('<tr>');
 
       row.forEach((tooths, toothsIndex) => {
         tooths.forEach((tooth) => {
           const idTooth: IdTooth = `${
             rowIndex + rowIndex + toothsIndex + 1
           }.${tooth}`;
-          //
-
-          text.push(`<td style="padding:5px, padding-bottom:15px">`);
-
-          text.push(`<div>`);
-          text.push(stateMouth[idTooth].top ? '🟩' : '⬜');
-          text.push(`</div>`);
-
-          text.push(`<div>`);
-          text.push(`${idTooth}`);
-          text.push(`</div>`);
-
-          text.push(`<div>`);
-          text.push(stateMouth[idTooth].bottom ? '❇️' : '⬜');
-          text.push(`</div>`);
-
-          text.push(`</td>`);
+          text.push(`<td style="padding: 5px;">
+            <div>
+              ${stateMouth[idTooth].top ? '🟩' : '⬜'}
+            </div>
+            <div>
+              ${idTooth}
+            </div>
+            <div>
+            ${stateMouth[idTooth].bottom ? '❇️' : '⬜'}
+            </div>
+          </td>`);
         });
       });
       text.push('</tr>');
@@ -58,127 +52,150 @@ const formatEmail = (
     gridMouth: GridMouth,
     rawData: FormValues
   ) => `
-  <table>
-  <tr style="border: 1px solid black">
-      <td>${fields.organization.title}</td>
-      <td>${rawData.organization}</td>
+  <table border="1" cellspacing="0" cellpadding="0">
+  <tr border="1">
+    <td border="0">${fields.organization.title}</td>
+    <td border="0">${rawData.organization}</td>
   </tr>
-  <tr style="border: 1px solid black">
-      <td>${fields.customerName.title}</td>
-      <td>${rawData.customerName}</td>
+  <tr border="1">
+    <td>${fields.customerName.title}</td>
+    <td>${rawData.customerName}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.phone.title}</td>
       <td>${rawData.phone}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.actualAddressDelivery.title}</td>
       <td>${rawData.actualAddressDelivery}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.communicationApp.title}</td>
       <td>${
         fields.communicationApp.options &&
         fields.communicationApp.options[rawData.communicationApp]
       }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.patientName.title}</td>
       <td>${rawData.patientName}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.operationDate.title}</td>
       <td>${rawData.operationDate}</td>
   </tr>
-  <tr style="border: 1px solid black">
-      <td>${fields.kindOfWork.title}</td>
-      <td>${
-        fields.kindOfWork.options &&
-        fields.kindOfWork.options[rawData.kindOfWork]
-      }</td>
+  <tr border="1">
+    <td>${fields.kindOfWork.title}</td>
+    <td>${
+      fields.kindOfWork.options && fields.kindOfWork.options[rawData.kindOfWork]
+    }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  ${
+    rawData.kindOfWork === '0' /* Хирургический набор */
+      ? `<tr border="1">
       <td>${fields.teethToggle.title}</td>
       <td>${rawData.teethToggle ? 'Да' : 'Нет'}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.toothColor.title}</td>
       <td>${
         fields.toothColor.options &&
         fields.toothColor.options[rawData.toothColor]
       }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.prosthesisType.title}</td>
       <td>${
         fields.prosthesisType.options &&
         fields.prosthesisType.options[rawData.prosthesisType]
       }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.reinforcementToggle.title}</td>
       <td>${rawData.reinforcementToggle ? 'Да' : 'Нет'}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.implantSystem.title}</td>
       <td>${rawData.implantSystem}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.implantProtocol.title}</td>
       <td>${
         fields.implantProtocol.options &&
         fields.implantProtocol.options[rawData.implantProtocol]
       }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.surgicalKit.title}</td>
       <td>${rawData.surgicalKit}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.pinSystem.title}</td>
       <td>${rawData.pinSystem}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.sleeveToggle.title}</td>
       <td>${rawData.sleeveToggle ? 'Да' : 'Нет'}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.surgkitRentalToggle.title}</td>
       <td>${rawData.surgkitRentalToggle ? 'Да' : 'Нет'}</td>
   </tr>
-  <tr style="border: 1px solid black">
+  ${
+    rawData.surgkitRentalToggle
+      ? `
+    <tr border="1">
       <td>${fields.implantGuides.title}</td>
       <td>${
         fields.implantGuides.options &&
         fields.implantGuides.options[rawData.implantGuides]
       }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  `
+      : ''
+  }
+  <tr border="1">
       <td>${fields.sourceFiles.title}</td>
       <td>${
         fields.sourceFiles.options &&
         fields.sourceFiles.options[rawData.sourceFiles]
       }</td>
   </tr>
-  <tr style="border: 1px solid black">
+  <tr border="1">
       <td>${fields.sourceFilesLink.title}</td>
       <td>${rawData.sourceFilesLink}</td>
-  </tr>
-  <tr style="border: 1px solid black">
+  </tr>`
+      : ''
+  }
+  <tr border="1">
       <td>${fields.comment.title}</td>
       <td>${rawData.comment}</td>
-  </tr style="border: 1px solid black">
+  </tr>
+  <tr border="1" >
+      <td colspan="2">
+          ${
+            rawData.kindOfWork === '0' /* Хирургический набор */
+              ? `<div>${fields.teethGrid.title}</div>${textGridMouth(
+                  gridMouth,
+                  rawData.teethGrid
+                )}`
+              : ''
+          }
+      </td>
+  </tr>
   </table>
-  <div>${fields.teethGrid.title}</div>
-  ${textGridMouth(gridMouth, rawData.teethGrid)}
+  <br />
   `;
 
-  return text(fields, gridMouth, rawData);
+  return text(fields, gridMouth, rawData).replace(/\s{2,}/g, ' ');
 };
 
 export const sendForm = (rawData: FormValues) => {
+  const payload = formatEmail(fields, mouth, rawData);
+  console.log(payload);
+
   return fetch(url, {
     method: 'POST',
-    body: formatEmail(fields, mouth, rawData),
+    body: payload,
   });
 };
